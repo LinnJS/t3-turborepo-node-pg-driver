9ea3d49dc647199f872a6148bb4283972cdba444
Justin Linn <justin@linnjs.com>
2024-12-12 11:49:41 -0500

    fix(format): Add explainitory comments back to next.config.js

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -1,12 +1,14 @@
 import createJiti from "jiti";
 import { fileURLToPath } from "url";
 
+// Import env files to validate at build time. Use jiti so we can load .ts files in here.
 createJiti(fileURLToPath(import.meta.url))("./src/env");
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
 
+  /** Enables hot reloading for local packages without a build step */
   transpilePackages: [
     "@acme/api",
     "@acme/auth",
@@ -15,11 +17,14 @@ const config = {
     "@acme/validators",
   ],
 
+  /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 
+  /** Output a standalone build for self hosting */
   output: "standalone",
 
+  /** Remove node.js specific imports from the build to suppress errors */
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
